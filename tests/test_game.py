5b5e165126cf461d560b670fb477ba1c2af9c33d
Jacob Fredenburg <jdfreden@ncsu.edu>
2021-04-11 09:27:10 -0400

    add tests folder

---
+++ tests/test_game.py
@@ -0,0 +1,18 @@
+from unittest import TestCase
+
+
+class TestSpadesState(TestCase):
+    def test_bet(self):
+        self.fail()
+
+    def test_get_next_player(self):
+        self.fail()
+
+    def test_do_move(self):
+        self.fail()
+
+    def test_score(self):
+        self.fail()
+
+    def test__update_score(self):
+        self.fail()
